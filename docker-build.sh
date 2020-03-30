fa46beb1b87e08e4c907c198326d6b31e2b0c020
Kristof Jozsa <kristof.jozsa@gmail.com>
2020-03-30 12:59:05 +0200

    fix docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,6 @@
 docker build -t paymenthubee.azurecr.io/phee/camunda-zeebe:0.23.0-alpha2 .
 docker push paymenthubee.azurecr.io/phee/camunda-zeebe:0.23.0-alpha2 
 
-docker tag -t paymenthubee.azurecr.io/phee/camunda-zeebe:latest paymenthubee.azurecr.io/phee/camunda-zeebe:0.23.0-alpha2
+docker tag paymenthubee.azurecr.io/phee/camunda-zeebe:0.23.0-alpha2 paymenthubee.azurecr.io/phee/camunda-zeebe:latest
 docker push paymenthubee.azurecr.io/phee/camunda-zeebe:latest
 
